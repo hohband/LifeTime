22c846ed5ae4a583bffa20bf05634ae4b35ba7d4
Li Li <hohband@qq.com>
2015-05-23 04:48:14 +0800

    First Try

--- LifeTime/ViewController.swift
+++ LifeTime/ViewController.swift
@@ -9,10 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // @IBOutlet weak var lifeProgress: UIProgressView!
+    @IBOutlet weak var lifeProgress: UIProgressView!
+    @IBOutlet weak var spentLabel: UILabel!
+    @IBOutlet weak var remainLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var now = NSDate()
+        
+        var dateBirthStr = "1971-06-10 04:00:00"
+        var ft = NSDateFormatter()
+        ft.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        
+        
+        var dateBirth = ft.dateFromString(dateBirthStr)
+        
+        var lifeYear = 80
+        
+        var spentSecond = now.timeIntervalSinceDate(dateBirth!)
+        
+        var supposeSecond = 80 * 365 * 24 * 60 * 60.0
+        
+        var remainSecond =  supposeSecond - spentSecond
+        
+        lifeProgress.progress = Float(spentSecond / supposeSecond)
+        spentLabel.text = "1"
+        remainLabel.text = "2"
+        
     }
 
     override func didReceiveMemoryWarning() {
--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -9,5 +9,25 @@ var now = NSDate()
 
 var dateBirthStr = "1971-06-10 04:00:00"
 var ft = NSDateFormatter()
+ft.dateFormat = "yyyy-MM-dd HH:mm:ss"
 
-var dateBirth = NSDate()
+
+var dateBirth = ft.dateFromString(dateBirthStr)
+
+var lifeYear = 80
+
+var spentSecond = now.timeIntervalSinceDate(dateBirth!)
+
+var supposeSecond = 80 * 365 * 24 * 60 * 60.0
+
+var remainSecond =  supposeSecond - spentSecond
+
+var f = Float(spentSecond / supposeSecond)
+
+
+
+
+
+
+
+
