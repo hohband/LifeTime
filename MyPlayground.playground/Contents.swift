ca83b45d6b2f2bebd1e8f633919edbe027a2409e
Li Li <hohband@qq.com>
2015-05-23 03:46:16 +0800

    Create

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+var now = NSDate()
+
+var dateBirthStr = "1971-06-10 04:00:00"
+var ft = NSDateFormatter()
+
+var dateBirth = NSDate()
